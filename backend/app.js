eb425721baa2a2407052eb76c3f97baa3784f328
Olga Kon <konolga@users.noreply.github.com>
2019-05-05 23:23:34 +0300

    deploy

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,9 @@ const path = require('path')
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const contactsRoutes = require("./routes/contacts");
+const dotenv = require('dotenv');
 
+dotenv.config();
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true }
   )
