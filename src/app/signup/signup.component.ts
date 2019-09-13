e9bb6e4e36d67791c0b29566f4a5498f2bfd1925
konolga <olgakon.mail@gmail.com>
2019-09-13 16:44:32 +0300

    password frontend validation

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -21,11 +21,16 @@ user: SocialUser;
 loggedIn: boolean;
 
   email = new FormControl('', [Validators.required, Validators.email]);
-  constructor(private authService: AuthService,
-              private alertify: AlertifyService,
-              private socialAuthService: SocialAuthService,
-              private fbAuthService: FbAuthService,
-              private router: Router) { }
+  password = new FormControl('',[Validators.required,
+                                 Validators.minLength(6),
+                                 Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]);
+        // this is for the letters (both uppercase and lowercase) and numbers validation
+
+        constructor(private authService: AuthService,
+                    private alertify: AlertifyService,
+                    private socialAuthService: SocialAuthService,
+                    private fbAuthService: FbAuthService,
+                    private router: Router) { }
 
   ngOnInit() {
     this.socialAuthService.authState.subscribe((user) => {
@@ -34,11 +39,18 @@ loggedIn: boolean;
       console.log(this.user);
    });
   }
-  getErrorMessage() {
+  getEmailErrorMessage() {
     return this.email.hasError('required') ? 'You must enter a value' :
         this.email.hasError('email') ? 'Not a valid email' :
             '';
   }
+  getPasswordErrorMessage() {
+    return this.password.hasError('required') ? 'You must enter a value' :
+           this.password.hasError('minLength') ? 'Password must be at least 6 characters long' :
+           this.password.hasError('pattern') ? 'Your password must contain at least one uppercase, one lowercase, and one number' :
+           '';
+  }
+
   signUp() {
    this.authService.signup(this.model).subscribe(() => {
     this.alertify.success('signed up successfully');
@@ -53,7 +65,7 @@ loggedIn: boolean;
 
 facebookSignIn() {
 this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID).then(userData => {
-  this.fbAuthService.socialLogin(userData).subscribe(response=>{
+  this.fbAuthService.socialLogin(userData).subscribe(response => {
     this.markAsRegistered.emit(true);
     this.alertify.success('signed up successfully');
     this.cancel();
